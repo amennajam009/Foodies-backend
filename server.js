ca94343fff80685d8afc3f51584bec0136a332c2
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-04-02 22:11:59 +0500

    add

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const cors= require('cors')
-const LoadMyEnviormentVariables = require('./controller/LoadEnvironmentvariable');
+const LoadMyEnviormentVariables = require('./configuration/LoadEnv-variable');
 require('dotenv').config();
 
 
@@ -9,20 +9,17 @@ require('dotenv').config();
 // //Block Start Initialize the app and Creating app mete-data
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-// hum edhr json icliye use kr rhy han kyu k yeh space kaam leta hai cuz yeh string may hai toh yeh humry data ko parse kr k string may convert krdyga taak 
-// data storage kaam use ho
 app.use(express.json());
 app.use(express.text());
 app.use(express.raw());
 app.use(cors());
-// const PORT = process.env.PORT;
 // static ka matlb hai icko hum access kr paa rhy han 
 app.use('/assets', express.static('assets')); 
 const PORT = process.env.PORT || 6000
 
 
 app.all('*', (req, res, next) => {
-    // This is how we protect the api
+   
     res.header('Access-Control-Allow-Origin', '*');// So it make the header allow to the origin when cross platfrom try to exchange the data
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
