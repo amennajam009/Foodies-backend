ae1f0a8cbde1e069e2a16412f520e3eaf9880114
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-04-02 22:01:58 +0500

    add

---
+++ controller/LoadEnvironmentvariable.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+let MyEnviroment = {};
+
+if(process.env.NODE_ENV === 'development'){
+    MyEnviroment = dotenv.config({path:`${__dirname}/../development.env`});
+}
+
+
+
+console.log(MyEnviroment);
