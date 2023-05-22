e3d846d2e9cdf390951768dcd1dee0837590c169
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-22 23:03:10 +0500

    add

--- Route/Contact.js
+++ Route/Contact.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const Router =  express.Router();
 
+const {
+    OrderTableApi 
+} = require('../controller/Contact')
 
 
-
+Router.post('/OrderTableApi',OrderTableApi)
 
 module.exports = Router;
---
+++ controller/Contact.js
@@ -0,0 +1,26 @@
+const {OrderTable}  = require('../model/ContactModel');
+
+
+const OrderTableApi = async(req,res) =>{
+    try {
+        const GetUserBookingTable = await OrderTable.save(req.body);
+        res.json({
+            message:'Api works',
+            Data:true,
+            Result:GetUserBookingTable
+        })
+
+    } catch (error) {
+        res.json({
+            message:error.message,
+            Data:false,
+            Result:null
+        })
+    }
+}
+
+
+
+module.exports={
+OrderTableApi
+}
