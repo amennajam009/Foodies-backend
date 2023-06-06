b247ce21cf42a0be0cfd783df12369731ed5fc5a
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-06-06 20:52:34 +0500

    add

--- Route/Whatsapp.js
+++ Route/Whatsapp.js
@@ -16,3 +16,41 @@ Router.post('/WhatsappApi',WhatsappApi)
 
 
 module.exports = Router;
+
+
+
+
+
+// const AdminRegister= async(req,res) => {                                                       
+//     try {
+//         const {FirstName, LastName, Email, Password} = req.body;                              
+//         const _GetAdminUserLength = await _AdminManagementModel.find();                              
+//         if (_GetAdminUserLength.length >= 2) {
+//             res.json({
+//                 message:`Admin Regesteration is Constraint`,
+//                 Status:null,
+//                 Data:false
+//             })
+//         } else {
+//             const _RegisterAdmin = new _AdminManagementModel({
+//                 FirstName,
+//                 LastName ,
+//                 Email,
+//                 Password
+                            
+//             });
+//             await _RegisterAdmin.save();
+//             res.json({
+//                 message:`User Register Successfully`,
+//                 data:true,
+//                 result:_RegisterAdmin
+//             })
+//         }
+//     } catch (error) {
+//         res.json({ 
+//             message: error.message, 
+//             result: false, 
+//             data:false 
+//         });
+//     }
+// }
