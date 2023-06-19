18460ccf3ec9c56aeb5966639512796a3f366fb5
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-06-19 14:30:46 +0500

    add

--- model/UserRegister.js
+++ model/UserRegister.js
@@ -3,44 +3,60 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const SaltRounds = 10;
 
-// Date
-const today = new Date();
-const day = today.getDate();
-const month = today.getMonth() + 1;
-const year = today.getFullYear();
-const time = today.getTime();
 
 //Start Block Schema Creating
 const UserRegisterSchema = mongoose.Schema({
     firstName: { type: String, required: true},
     lastName: { type: String, required: true},
     // email: { type: String, required: true, unique:true},
-    Email: { type: String, required: true},
+    Email: { type: String, required: true ,unique:true},
     Password: { type: String, required: true},
     saltString: { type:String},
     status: { type:Number, default:1 },
-    CreatedDate: {
-        type: String,
-        default: `${year}-${month}-${day}-${time}`,
-    }
-})
+},{timestamps:true})
 
-UserRegisterSchema.pre('save', function(next){
-    bcrypt.genSalt(SaltRounds,(err,salt)=>{
-        if(salt){
-        this.saltString=salt;
-        bcrypt.hash(this.Password,salt,(err,hash)=>{
-            this.Password=hash;
-            next();
-        })
+// UserRegisterSchema.pre('save', function(next){
+//     bcrypt.genSalt(SaltRounds,(err,salt)=>{
+//         if(salt){
+//         this.saltString=salt;
+//         bcrypt.hash(this.Password,salt,(err,hash)=>{
+//             this.Password=hash;
+//             next();
+//         })
+//     }
+//     else {
+//         res.json({
+//             Error:err.message
+//         })
+//     }
+//     })
+// });
+
+
+UserRegisterSchema.pre('save', function (next) {
+    if (!this.Email) {
+      return next(new Error('Email is required'));
     }
-    else {
-        res.json({
-            Error:err.message
-        })
+  
+    if (!this.Password) {
+      return next(new Error('Password is required'));
     }
-    })
-});
+  
+    bcrypt.genSalt(SaltRounds, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+  
+      this.saltString = salt;
+      bcrypt.hash(this.Password, salt, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        this.Password = hash;
+        next();
+      });
+    });
+  });
 //End Block Schema Creating
 
 //Exporting The Schema
