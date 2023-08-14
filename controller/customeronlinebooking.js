56f47070e83369c7ea4b2c66c9be322382453c70
amennajam009 <amennajam7@gmail.com>
2023-08-14 19:00:55 +0500

    add

--- controller/customeronlinebooking.js
+++ controller/customeronlinebooking.js
@@ -3,8 +3,33 @@ const {CustomerOnlineBooling}  = require('../model/customeronlinebooking');
 
 const CustomerOnlineBookingAPI =async (req,res) =>{
     try {
-        const {Username,Emailaddress,FirstName,LastName,City} =req.body
+        const {Username,Emailaddress,FirstName,LastName,City} =req.body;
+        const MappingData = await CustomerOnlineBooling.save({
+            Username,
+            Emailaddress,
+            FirstName,
+            LastName,
+            City,
+            imageDetails: {
+                imageUrl: `assets/customers/${FirstName}/${req.file.filename}`,
+                imageName: req.file.originalname,
+                imageMimeType: req.file.mimetype,
+            }
+        });
+        res.json({
+            result:MappingData,
+            message:'Saved Successfully!!',
+            data:true
+        })
     } catch (error) {
-        
+        res.json({
+            result:null,
+            message:error.message,
+            data:false,
+        })
     }
 } 
+
+module.exports={
+   CustomerOnlineBookingAPI
+    }
