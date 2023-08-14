d0611f8dab41f164dbdd59d420ae1888af501640
amennajam009 <amennajam7@gmail.com>
2023-08-14 15:53:52 +0500

    add

--- model/customeronlinebooking.js
+++ model/customeronlinebooking.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 
 
 const customeronlinebooking = new mongoose.Schema({
-    YourName: { type:String, required:true },
-    YourEmail:{type:String, required:true},
-    YourPhone:{type:String, required:true},
-    Date:{type:String, required:true},
-    Time:{type:String, required:true},
-    People:{type:Number, required:true},
-    Message:{type:String, required:true},
-    status: { type: Number, default: 1 },
+    Username: { type:String, required:true },
+    Emailaddress:{type:String, required:true},
+    FirstName:{type:String, required:true},
+    LastName:{type:String, required:true},
+    City:{type:String, required:true},
+    // People:{type:Number, required:true},
+    // Message:{type:String, required:true},
+    // status: { type: Number, default: 1 },
     
 },
  { timestamps: true });
