481718654faa757b1ab3789d77cd6cd715ff45c2
amennajam009 <amennajam7@gmail.com>
2023-08-14 16:06:59 +0500

    add

---
+++ controller/customeronlinebooking.js
@@ -0,0 +1,10 @@
+const {CustomerOnlineBooling}  = require('../model/customeronlinebooking');
+
+
+const CustomerOnlineBookingAPI =async (req,res) =>{
+    try {
+        const {Username,Emailaddress,FirstName,LastName,City} =req.body
+    } catch (error) {
+        
+    }
+} 
